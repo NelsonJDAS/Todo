74cf1adeee14d7dab77d252128e8e27ce447e03e
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-10-31 11:49:37 +0000

    first commit

---
+++ src/js/component/Header.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const Header = (props) => {
+  return (
+    <div
+      className={
+        props.tema === "dark"
+          ? "row pt-3 bg-black text-white border-bottom border-3 border-white"
+          : "row pt-3 bg-white text-white border-bottom border-3 border-dark"
+      }
+    >
+      <div className="col-6 col-md-8 col-lg-10 fw-bold fs-1">
+        <h1
+          className={
+            props.tema === "dark" ? "fs-1 mx-3" : "fs-1 mx-3 text-black"
+          }
+        >
+          Todo<span className="text-warning">.List</span>
+        </h1>
+      </div>
+      <div className="col-md-4 col-lg-2 col-6 text-end pb-3">
+        <div className="mx-2">
+          <button
+            className="btn fw-bold m-0 p-0"
+            onClick={() => {
+              props.setTema("dark");
+            }}
+          >
+            <i
+              class={
+                props.tema === "dark"
+                  ? "fa-regular fa-moon selected-dark px-3 border border-white text-white border-end-1 mx-2"
+                  : "fa-regular fa-moon selected-dark px-3 border border-dark mx-2"
+              }
+            ></i>
+          </button>
+          <button
+            className="btn fw-bold m-0 p-0"
+            onClick={() => {
+              props.setTema("white");
+            }}
+          >
+            <i
+              class={
+                props.tema === "white"
+                  ? "fa-regular fa-sun text-white selected-white px-3 border border-dark"
+                  : "fa-regular fa-sun text-white selected-white px-3 border border-white"
+              }
+            ></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Header from "./Header";
 
 //create your first component
 const Home = () => {
-	return (
-		
-	);
+  const [tema, setTema] = useState("dark");
+  return (
+    <>
+      <Header tema={tema} setTema={setTema} />
+      <div
+        className={
+          tema === "dark" ? "bg-dark text-white" : "bg-white text-dark"
+        }
+      >
+        <div className="container-fluid">
+          <div className="row text-center">
+            <h2 className="fw-bold text-warning">Tareas</h2>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-2">
+                <span>1</span>
+              </div>
+              <div className="col-4">
+                <p>tarea de prueba aaaaaaaaaa</p>
+              </div>
+              <div className="col-4">
+                <p>creador el 00/ 00/ 00/</p>
+              </div>
+              <div className="col-2">
+                <i class="fa-solid fa-xmark text-danger fw-bold"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
