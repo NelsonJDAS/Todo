99545cdfea5ba4de29cb18e35a53e28235cec1c1
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-10-31 22:16:26 +0000

    Js Added 1/2

--- src/js/component/Main.jsx
+++ src/js/component/Main.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Task from "./Task";
 
 const Main = (props) => {
+  const [ID, SetID] = useState(1);
+  const [TodoTask, SetTodoTask] = useState([]);
+  const [Input, setInput] = useState("");
+  const inputRef = useRef(null);
+
+  const handleElementoString = (e) => {
+    setInput(e.target.value);
+  };
+
+  const DateC = () => {
+    const CreateDate = new Date();
+    return `${CreateDate.getDate()}/${
+      CreateDate.getMonth() + 1
+    }/${CreateDate.getFullYear()}`;
+  };
+
+  const AddTask = (ID, DONE, TASK, DATE) => {
+    SetTodoTask([
+      ...TodoTask,
+      { id: ID, Done: DONE, TaskText: TASK, DateCreate: DATE },
+    ]);
+    inputRef.current.value = "";
+    inputRef.current.placeholder =
+      elem === "" ? "Ninguna tarea fue introducida" : "Todo agregado";
+  };
   return (
     <main
       className={
@@ -25,6 +50,8 @@ const Main = (props) => {
                     : "form-control rounded-0 w-100 py-3 rounded-start text-center fw-bold task-container-white border border-dark"
                 }
                 placeholder="Introduce tu tarea aqui!!!"
+                onChange={handleElementoString}
+                ref={inputRef}
               />
             </div>
             <div className="col-md-2 col-4 p-0 text-start">
@@ -34,6 +61,11 @@ const Main = (props) => {
                     ? "btn btn-outline-light rounded-0 w-100 py-3 rounded-end text-white border-start-0 boton"
                     : "btn btn-outline-dark rounded-0 w-100 py-3 rounded-end border border-dark border-start-0"
                 }
+                onClick={() => {
+                  AddTask(ID, false, Input, DateC());
+                  SetID(ID + 1);
+                }}
+                d
               >
                 Subir Tarea
               </button>
@@ -47,30 +79,16 @@ const Main = (props) => {
               : "container task-container-white border border-dark container-task rounded-2"
           }
         >
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
+          {TodoTask.map((elem) => {
+            return (
+              <Task
+                done={elem.Done}
+                id={elem.id}
+                TodoText={elem.TaskText}
+                date={elem.DateCreate}
+              />
+            );
+          })}
         </div>
       </div>
     </main>
--- src/js/component/Task.jsx
+++ src/js/component/Task.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 
-const Task = () => {
-  const FechaDeCreacion = new Date();
-
+const Task = (props) => {
   return (
     <div className="row border border-white py-3">
       <div className="col-md-1 col-1 text-center align-content-center">
         <i class="fa-regular fa-circle task "></i>
       </div>
       <div className="col-md-1 col-1 text-center ml-2 align-content-center">
-        <span className="fw-bold fs-3 mx-2">1</span>
+        <span className="fw-bold fs-3 mx-2">{props.id}</span>
       </div>
       <div className="col-md-6 col-4 align-content-center text-center">
-        <p>tarea de prueba aaaaaaaaa</p>
+        <p>{props.TodoText}</p>
       </div>
       <div className="col-md-3 col-3 align-content-center text-center">
-        <p>creador el 00/00/2000</p>
+        <p>{props.date}</p>
       </div>
       <div className="col-md-1 col-3 align-content-center text-end">
         <i class="fa-solid fa-xmark text-danger fw-bold mx-2 task"></i>
